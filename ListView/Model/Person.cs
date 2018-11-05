0fa5c1d5b9c1aaf09f0a2aeec638d9b5f132e5a4
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-05 13:52:05 +0100

    Final Release

---
+++ ListView/Model/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView.Model
+{
+    class Person
+    {
+        private int _id;
+        private int _age;
+        private string _name;
+
+        public Person(int id, int age, string name)
+        {
+            _id = id;
+            _age = age;
+            _name = name;
+        }
+
+        public int Id
+        {
+            get { return _id;}
+            set { _id = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+    }
+}
---
+++ ListView/ViewModel/PersonVm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListView.Model;
+
+namespace ListView.ViewModel
+{
+    class PersonVm
+    {
+        public List<Person> ListPerson { get; set; }
+
+        public PersonVm()
+        {
+            ListPerson = new List<Person>()
+            {
+                new Person(123, 21 , "Zuhair"),
+                new Person(123, 45 , "Petro"),
+                new Person(123, 56 , "Lako")
+
+            };
+        }
+
+    }
+}
